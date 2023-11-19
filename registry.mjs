e3001ea432e1e17cec42a32f2f1ad01b1db3d164
marchuanv <marchuanv@gmail.com>
2023-11-19 11:29:46 +0200

    cleanup

--- registry.mjs
+++ registry.mjs
@@ -1,10 +1,9 @@
 import Jasmine from 'jasmine';
 import { Properties } from './lib/properties.mjs';
 import { Relation } from './lib/relation.mjs';
-export { EventEmitter } from 'events';
 export { constants, createHmac, generateKeyPairSync, privateDecrypt, publicEncrypt, randomBytes, randomUUID } from 'node:crypto';
 export { Bag } from './lib/bag.mjs';
 export { Jasmine, Properties, Relation };
 export * as url from 'url';
-export { Specs } from 'utils';
+export { Specs, EventEmitter } from 'utils';
 export { BagItem } from './lib/bag.item.mjs';
