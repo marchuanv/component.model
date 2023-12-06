115a1ea875548636bf9eab13e24da15907f924f2
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-12-06 23:59:01 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -4,7 +4,7 @@ import { Properties } from './lib/properties.mjs';
 import { Relation } from './lib/relation.mjs';
 export { constants, createHmac, generateKeyPairSync, privateDecrypt, publicEncrypt, randomBytes, randomUUID } from 'node:crypto';
 export * as url from 'url';
-export { EventEmitter, Specs, reflection } from 'utils';
+export { ClassMember, EventEmitter, Specs } from 'utils';
 export { BagItem } from './lib/bag.item.mjs';
 export { Bag } from './lib/bag.mjs';
 export { CtorParam, Jasmine, Properties, Relation };
