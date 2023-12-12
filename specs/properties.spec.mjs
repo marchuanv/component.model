f54bea7de913d77a098daa3ae6fb521c1206f266
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-12-13 00:04:06 +0200

    refactor

--- specs/properties.spec.mjs
+++ specs/properties.spec.mjs
@@ -26,75 +26,31 @@ describe('when properties change', () => {
         expect(human.name).toBe(expectedName);
         expect(human.age).toBe(expectedAge);
     });
-    fit('should sync data within context', () => {
-        const contextRoot = new ContextRoot();
-        const actualValue = 'b4ad82a4-d76e-4b36-bc4d-f0ca5386622f';
-        contextRoot.Id = actualValue;
-        expect(contextRoot.Id).toBe(actualValue);
-        let firedCount = 0;
-        contextRoot.onSet({ Id: null }, false, false, (value) => {
-            firedCount = firedCount + 1;
-            return actualValue;
-        });
-        const expectedValue = '1deccbb7-b859-4e77-a1b5-7b5b58ce3b31';
-        contextRoot.Id = expectedValue; //onChange
-        expect(firedCount).toBe(1);
-        expect(contextRoot.Id).toBe(actualValue);
+    it('should serialise', async () => {
+        const expectedName = 'Bob';
+        const expectedAge = 30;
+        const human = new Human(expectedName, expectedAge, 170, 89);
+        expect(human.name).toBe(expectedName);
+        expect(human.age).toBe(expectedAge);
+
+        const serialised = await human.serialise();
+        expect(serialised).toBeDefined();
+        expect(serialised).not.toBeNull();
     });
-    it('should serialise', () => {
+    fit('should deserialise', async () => {
+        const expectedName = 'Bob';
+        const expectedAge = 30;
+        const human = new Human(expectedName, expectedAge, 170, 89);
+        expect(human.name).toBe(expectedName);
+        expect(human.age).toBe(expectedAge);
 
-        const expectedId = '484db6d7-7c17-46ce-9b57-888ce835edf7';
-        const expectedId1 = 'ef7cbdeb-6536-4e38-a9e1-cc1acdd00e7d';
-        const expectedId2 = '00584b74-bb02-4284-b40a-5c69c928cba9';
+        const serialised = await human.serialise();
+        expect(serialised).toBeDefined();
+        expect(serialised).not.toBeNull();
 
-        const contextRoot = new ContextRoot();
-        contextRoot.Id = expectedId;
-
-        const contextA = new ContextA({ contextRoot });
-        contextA.Id = expectedId1;
-
-        const contextB = new ContextB({ contextRoot });
-        contextB.Id = expectedId2;
-
-        const contextASerialised = contextA.serialise();
-        expect(contextASerialised).toBe(JSON.stringify({ contextRoot: { Id: expectedId }, Id: expectedId2 }));
-
-        const contextBSerialised = contextB.serialise();
-        expect(contextBSerialised).toBe(JSON.stringify({ contextRoot: { Id: expectedId }, Id: expectedId2 }));
-    });
-    it('should share properties with same context', () => {
-
-        const contextRoot = new ContextRoot();
-        contextRoot.Id = '653ef45a-14ba-400b-a1a9-c0695d6b1f06';
-
-        const contextA = new ContextA({ contextRoot });
-        contextA.Id = '250b70e1-fe1f-47eb-8185-04278ddef1bc';
-
-        const contextB = new ContextB({ contextA });
-        contextB.Id = 'c0785886-6652-4308-aab5-b96b15eb942e';
-
-        const contextC = new ContextC({ contextA });
-        contextC.Id = 'a70b6d9a-6e3b-40d3-a1b5-08327d1cd6e2';
-
-        expect(contextRoot.Id).not.toBe(contextA.Id);
-        expect(contextRoot.Id).not.toBe(contextB.Id);
-        expect(contextRoot.Id).not.toBe(contextC.Id);
-
-        expect(contextA.Id).not.toBe(contextB.Id);
-        expect(contextA.Id).not.toBe(contextC.Id);
-
-        expect(contextB.Id).toBe(contextC.Id); //they share a context
-        expect(contextC.Id).toBe(contextB.Id); //they share a context
-    });
-    it('should deserialise', async () => {
-
-        const contextD = new ContextD('Hello World')
-        contextD.Id = 'e742b112-1363-49f2-84dd-5e2e2c8dabe5';
-
-        const deserialised = await contextD.deserialise();
-        
-        expect(deserialised).toBeDefined();
-        expect(deserialised.Id).not.toBe(contextD.Id);
+        const humanDeserialised = await Properties.deserialise(serialised, Human);
+        expect(humanDeserialised).toBeDefined();
+        expect(humanDeserialised).not.toBeNull();
 
     });
 });
