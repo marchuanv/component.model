9d123ea7fc0f236e95eecd92b41494b39d4d1707
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-01-09 11:39:20 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -1,5 +1,5 @@
 export { constants, createHmac, generateKeyPairSync, privateDecrypt, publicEncrypt, randomBytes, randomUUID } from 'node:crypto';
 export * as url from 'url';
-export { ClassMember, Container, MemberParameter, PrimitiveType, ReferenceType, Specs, TypeMapper } from 'utils';
+export { ClassMember, Container, MemberParameter, PrimitiveType, PropertyMember, ReferenceType, Serialiser, Specs, TypeMapper } from 'utils';
 export { Model } from './lib/model.mjs';
 
